6a1a45c72b706810445d0f3b793fabf272047a2d
ikromyusupov <@ikromyusupovdev@gmail.com>
2022-12-24 16:11:13 +0500

    01 fixed

--- While01.py
+++ While01.py
@@ -1,6 +1,6 @@
 def main(s):
     """
-    A variable of type str is given. Find how many numbers it contains and return.
+    A variable of type str is given. Find how many digits it contains and return.
     Args:
         s: str
     Returns:
