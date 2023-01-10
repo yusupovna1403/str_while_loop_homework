f5270ee162c3ed8fdb04e2929ab4e287ed744893
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-10 15:12:24 +0500

    while_loop_06

--- While06.py
+++ While06.py
@@ -1,3 +1,4 @@
+import string
 def main(s):
     """
     A variable of type str is given. Find and return how many consonant letters there are.
@@ -7,3 +8,11 @@ def main(s):
     Returns:
         int: return answer
     """
+    i = 0
+    count = 0
+    while i < len(s):
+        if s[i] != 'a' and s[i] != 'e' and s[i] != 'i' and s[i] != 'o' and s[i] != 'u' and s[i] != 'A' and s[i] != 'E' and s[i] != 'I' and s[i] != 'O' and s[i] != 'U' and s[i].isspace()==0 and s[i] not in string.punctuation and s[i].isdigit()==0:
+            count+=1
+        i+=1
+    return count
+print(main("Codeschooluz"))
