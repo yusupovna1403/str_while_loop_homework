83d9150dcffb17ba8370037b5bbbdea88fb28741
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-10 16:14:50 +0500

    while_loop_10

--- While10.py
+++ While10.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    count = 0
+    while i < len(s):
+        if s[i] >= '0' and s[i] <= '9':
+            if int(s[i]) % 2 == 1:
+                count+=int(s[i])
+        
+        i+=1
+    return count
+print(main('python 20221178'))
